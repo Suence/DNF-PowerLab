6b902cb5bd3ba11be1adacacf6ce5b623526eb45
Suence <Suence.Online@outlook.com>
2024-07-25 18:44:27 +0800

    :building_construction: 添加基本项目结构

---
+++ SRC/PowerLab.Core/Mvvm/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace PowerLab.Core.Mvvm
+{
+    public abstract class ViewModelBase : BindableBase, IDestructible
+    {
+        protected ViewModelBase()
+        {
+
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
---
+++ SRC/Services/PowerLab.Services.Interfaces/IMessageService.cs
@@ -0,0 +1,7 @@
+﻿namespace PowerLab.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        string GetMessage();
+    }
+}
