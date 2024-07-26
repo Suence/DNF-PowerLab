f55edebb851790dfb54031c6ae1abe25c476ccd9
Suence <Suence.Online@outlook.com>
2024-07-26 17:57:38 +0800

    阻止应用程序多实例

---
+++ SRC/PowerLab.Core/Native/_Win32/Win32Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PowerLab.Core.Native.Win32
+{
+    public static class Win32Helper
+    {
+        /// <summary>
+        /// 查找窗口句柄
+        /// </summary>
+        /// <param name="lpClassName">ClassName</param>
+        /// <param name="lpWindowName">WindowName</param>
+        /// <returns>窗口句柄</returns>
+        [DllImport("USER32.DLL", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        /// <summary>
+        /// 通过窗口句柄激活窗口
+        /// </summary>
+        /// <param name="hWnd">窗口句柄</param>
+        /// <returns></returns>
+        [DllImport("USER32.DLL")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+}
--- SRC/PowerLab/App.xaml.cs
+++ SRC/PowerLab/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
+using PowerLab.Core.Native.Win32;
+using PowerLab.Core.Tools;
 using PowerLab.Modules.ModuleName;
 using PowerLab.Services;
 using PowerLab.Services.Interfaces;
@@ -26,6 +30,40 @@ namespace PowerLab
         protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
         {
             moduleCatalog.AddModule<ModuleNameModule>();
+        }
+
+        /// <summary>
+        /// 程序入口函数
+        /// </summary>
+        [STAThread] static void Main()
+        {
+            if (CheckAppIsRunning()) return;
+
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        /// <summary>
+        /// 检查程序是否已经运行
+        /// </summary>
+        /// <returns>True: </returns>
+        private static bool CheckAppIsRunning()
+        {
+            using var mutex = new Mutex(true, "E2A4C483-C59D-4856-BE14-F9B4AF07042C");
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                IntPtr mainWindowHandle = Win32Helper.FindWindow(null, ApplicationResources.GetString("AppName"));
+
+                if (mainWindowHandle != IntPtr.Zero)
+                {
+                    Win32Helper.SetForegroundWindow(mainWindowHandle);
+                }
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
